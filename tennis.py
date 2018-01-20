c6a6001342857c1c3229cffaf938bd5d32b6e05e
Simon Stead <simonstead@me.com>
2018-01-20 13:34:00 +0000

    Add SET state machine. Works, but needs massive refactor

--- tennis.py
+++ tennis.py
@@ -1,18 +1,28 @@
-from state_machine import state_machine
+from state_machine import state_machine, set_state_machine
 from events import *
 
 class TennisMachine():
     """a deterministic finite state automaton for Tennis scoring."""
     def __init__(self):
-        self.event_store = []
+        self.event_store_for_game = []
+        self.event_store_for_set = []
+        self.event_store_for_match = []
 
     def event(self, e):
-        self.event_store.append(e)
-        return len(self.event_store)
+        self.event_store_for_game.append(e)
+        return len(self.event_store_for_game)
 
-    def state(self, events=None, current_state=(0,0)):
+    def event_set(self, e):
+        self.event_store_for_set.append(e)
+        return len(self.event_store_for_set)
+
+    def event_match(self, e):
+        self.event_store_for_set.append(e)
+        return len(self.event_store_for_match)
+
+    def game_state(self, events=None, current_state=(0,0)):
         if events is None:
-            events = self.event_store
+            events = self.event_store_for_game
 
         if GAME_TO_PLAYER_ONE in state_machine[current_state]:
             return state_machine[current_state][GAME_TO_PLAYER_ONE]
@@ -25,4 +35,25 @@ class TennisMachine():
         new_state = state_machine[current_state][event]
         events = events[1:]
 
-        return self.state(events, new_state)
+        return self.game_state(events, new_state)
+
+    def set_state(self, events=None, current_state_game=(0,0), current_state_set=(0,0)):
+        if events is None:
+            events = self.event_store_for_game
+
+        if GAME_TO_PLAYER_ONE in state_machine[current_state_game]:
+            current_state_set = set_state_machine[current_state_set][GAME_TO_PLAYER_ONE]
+            new_state_game = state_machine[current_state_game][GAME_TO_PLAYER_ONE]
+            return self.set_state(events, new_state_game, current_state_set)
+        if GAME_TO_PLAYER_TWO in state_machine[current_state_game]:
+            current_state_set = set_state_machine[current_state_set][GAME_TO_PLAYER_TWO]
+            new_state_game = state_machine[current_state_game][GAME_TO_PLAYER_TWO]
+            return self.set_state(events, new_state_game, current_state_set)
+        if events == []:
+            return current_state_set
+
+        event = events[0]
+        new_state_game = state_machine[current_state_game][event]
+        events = events[1:]
+
+        return self.set_state(events, new_state_game, current_state_set)
--- test.py
+++ test.py
@@ -2,22 +2,22 @@ import unittest
 from tennis import TennisMachine
 from events import *
 
-class Tennis(unittest.TestCase):
+class TennisGameTests(unittest.TestCase):
     def test_initial_state(self):
         tennis = TennisMachine()
-        self.assertEqual(tennis.state(), (0,0))
+        self.assertEqual(tennis.game_state(), (0,0))
 
     def test_player_one_can_score(self):
         tennis = TennisMachine()
         tennis.event(PLAYER_ONE_SCORED)
-        score = tennis.state()
+        score = tennis.game_state()
         self.assertEqual(score, (1,0))
 
     def test_player_one_can_score_twice(self):
         tennis = TennisMachine()
         tennis.event(PLAYER_ONE_SCORED)
         tennis.event(PLAYER_ONE_SCORED)
-        score = tennis.state()
+        score = tennis.game_state()
         self.assertEqual(score, (2,0))
 
     def test_player_one_can_win_and_game_resets(self):
@@ -26,7 +26,7 @@ class Tennis(unittest.TestCase):
         tennis.event(PLAYER_ONE_SCORED)
         tennis.event(PLAYER_ONE_SCORED)
         tennis.event(PLAYER_ONE_SCORED)
-        score = tennis.state()
+        score = tennis.game_state()
         self.assertEqual(score, (0,0))
 
     def test_can_deuce_advantage_deuce(self):
@@ -41,5 +41,46 @@ class Tennis(unittest.TestCase):
         tennis.event(PLAYER_TWO_SCORED)
         tennis.event(PLAYER_ONE_SCORED)
         tennis.event(PLAYER_TWO_SCORED)
-        score = tennis.state()
+        score = tennis.game_state()
         self.assertEqual(score, (3,3))
+
+class TennisSetTests(unittest.TestCase):
+    def test_can_count_a_set(self):
+        tennis = TennisMachine()
+        self.assertEqual(tennis.set_state(), (0,0))
+
+    def test_can_win_a_set(self):
+        tennis = TennisMachine()
+        tennis.event(PLAYER_ONE_SCORED)
+        tennis.event(PLAYER_ONE_SCORED)
+        tennis.event(PLAYER_ONE_SCORED)
+        tennis.event(PLAYER_ONE_SCORED)
+        score = tennis.set_state()
+        self.assertEqual(score, (1,0))
+
+    def test_can_win_one_set_each(self):
+        tennis = TennisMachine()
+        tennis.event(PLAYER_ONE_SCORED)
+        tennis.event(PLAYER_ONE_SCORED)
+        tennis.event(PLAYER_ONE_SCORED)
+        tennis.event(PLAYER_ONE_SCORED)
+        tennis.event(PLAYER_TWO_SCORED)
+        tennis.event(PLAYER_TWO_SCORED)
+        tennis.event(PLAYER_TWO_SCORED)
+        tennis.event(PLAYER_TWO_SCORED)
+        score = tennis.set_state()
+        self.assertEqual(score, (1,1))
+
+    def test_can_win_six_sets_each(self):
+        tennis = TennisMachine()
+        for i in range(6):
+            tennis.event(PLAYER_ONE_SCORED)
+            tennis.event(PLAYER_ONE_SCORED)
+            tennis.event(PLAYER_ONE_SCORED)
+            tennis.event(PLAYER_ONE_SCORED)
+            tennis.event(PLAYER_TWO_SCORED)
+            tennis.event(PLAYER_TWO_SCORED)
+            tennis.event(PLAYER_TWO_SCORED)
+            tennis.event(PLAYER_TWO_SCORED)
+        score = tennis.set_state()
+        self.assertEqual(score, (6,6))
